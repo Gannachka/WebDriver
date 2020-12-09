b0ce5575e92207fa5e6afdfa6471ba9d95f01e52
Hanna Miki <immeditate@gmail.com>
2020-12-09 15:16:02 +0300

    Refactor test

--- src/test/java/Relui_test.java
+++ src/test/java/Relui_test.java
@@ -1,24 +1,22 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.junit.Assert;
 
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class Relui_test {
 
     public static String expectedResult = "COMPLIMENTI! СУПЕРОБЪЕМНАЯ" ;
     WebDriver driver = new EdgeDriver();
 
-    @BeforeMethod(alwaysRun = true)
+    @Before()
     public void BrowserSetup(){
         driver=new EdgeDriver();
     }
@@ -27,24 +25,24 @@ public class Relui_test {
     public void AddingItemsToCartTest() throws InterruptedException {
 
         driver.get("https://relouis.by/collection/complimenti/");
-        WebElement chooseProduct = driver.findElement(By.xpath("//div[@datatype=\"4810438013055\"]"));
+        WebElement chooseProduct = waitForElementLocatedBy(driver, By.xpath("//div[@datatype=\"4810438013055\"]"));
         chooseProduct.click();
         WebElement goToCart = waitForElementLocatedBy(driver, By.linkText("Перейти в корзину"));
         goToCart.click();
 
-        Assert.assertEquals(waitForElementLocatedBy(driver,By.xpath("//div[@class='cart-block-item-name']")).getText(), expectedResult);
+        Assert.assertFalse("Ваша корзина пуста.", !driver.findElement(By.xpath("//*[@id=\"cart-block\"]/div/div")).isDisplayed());
 
     }
 
 
-    @AfterMethod(alwaysRun = true)
+    @After()
     public void driverShutDown(){
         driver.quit();
         driver=null;
     }
 
     private static WebElement waitForElementLocatedBy(WebDriver driver, By by){
-        return new WebDriverWait(driver,4)
+        return new WebDriverWait(driver,50)
                 .until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
